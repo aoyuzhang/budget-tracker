1db62cceec7a73718b5e26c26727a8c319804754
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-23 06:05:17 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList'
+import AddExpenseForm from './components/AddExpenseForm';
 
 const App = () =>{
   return (
@@ -20,12 +21,21 @@ const App = () =>{
           <ExpenseTotal />
         </div>
       </div>
+
       <h3 className ='mt-3'>Expenses</h3>
       <div className = 'row mt-3'>
         <div className = 'col-sm'>
           <ExpenseList />
         </div>
       </div>
+
+      <h3 className = 'mt-3'>Add Expense</h3>
+      <div className = 'row mt-3'>
+        <div className = 'col-sm'>
+          <AddExpenseForm />
+        </div>
+      </div>
+
     </div>
   );
 };
